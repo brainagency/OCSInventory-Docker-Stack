c1f25f2c9b27b4494ef4792a5e070b6f23d87663
Alex Komis <oleksandr.komisaruk@brain-agency.com>
2017-06-07 14:57:02 +0300

    setup.sh improvement

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env bash
+# Downloading submodules
+git submodule init
+git submodule update
 
-docker-compose build
-docker-compose up
+# Symlink for ocsinventoryng plugin
+ln -s $(readlink -f ocsinventoryng/) $(readlink -f glpi/plugins/ocsinventoryng/)
+
+# Staring docker stack
+docker-compose up --build -d
